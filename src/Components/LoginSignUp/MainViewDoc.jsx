ad1edfeb5c94a8430629a516359bab38803fe472
Karolina Wasyluk <261637@student.pwr.edu.pl>
2023-11-24 19:21:23 +0100

    wyświetlanie kalendarza pacjenta

--- src/Components/LoginSignUp/MainViewDoc.jsx
+++ src/Components/LoginSignUp/MainViewDoc.jsx
@@ -1,13 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { getFirestore, collection, getDocs, getDoc, doc } from "firebase/firestore";
+import { getFirestore, collection, getDocs, getDoc, doc, query, where } from "firebase/firestore";
 import { auth } from "../../firebaseConfig";
 import { signOut } from 'firebase/auth';
 import { Link } from "react-router-dom";
 import "./MainView.css";
 import "./MainViewDoc.css";
 import menu_icon from '../Assets/menu.png';
-
+import './CalendarStyles.css';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import moment from "moment";
+// Dodaj import dla momentLocalizer
+import 'moment/locale/pl'; // Dodaj import dla lokalizacji moment (np. polska)
 
 const MainViewDoc = () => {
     const navigate = useNavigate();
@@ -15,7 +20,7 @@ const MainViewDoc = () => {
     const [isSidebarOpen, setSidebarOpen] = useState(false);
     const [patients, setPatients] = useState([]);
     const [selectedPatient, setSelectedPatient] = useState(null);
-
+    const [patientEvents, setPatientEvents] = useState([]);
 
     useEffect(() => {
         const email = auth.currentUser.email;
@@ -35,6 +40,7 @@ const MainViewDoc = () => {
             .catch((error) => {
                 console.error("Błąd podczas pobierania danych z Firestore:", error);
             });
+
     }, []);
 
     const handleLogout = async () => {
@@ -50,10 +56,44 @@ const MainViewDoc = () => {
         setSidebarOpen(!isSidebarOpen);
     };
 
-    const handlePatientClick = (patient) => {
+    const handlePatientClick = async (patient) => {
         setSelectedPatient(patient);
-    };
 
+        try {
+            const db = getFirestore(auth.app);
+            const medicationsRef = collection(db, "checkedMedications");
+            const q = query(medicationsRef, where("email", "==", patient));
+
+            const querySnapshot = await getDocs(q);
+
+            const events = [];
+            querySnapshot.forEach((doc) => {
+                const medicationData = doc.data();
+                const { medicationName, checkedDate, checkedTime } = medicationData;
+
+                const dateParts = checkedDate.split("/");
+                const timeParts = checkedTime.split(":");
+                const start = new Date(
+                    parseInt(dateParts[2]),
+                    parseInt(dateParts[1]) - 1,
+                    parseInt(dateParts[0]),
+                    parseInt(timeParts[0]),
+                    parseInt(timeParts[1])
+                );
+                const end = new Date(start.getTime() + 60 * 60 * 1000);
+
+                events.push({
+                    title: medicationName,
+                    start,
+                    end,
+                });
+            });
+
+            setPatientEvents(events);
+        } catch (error) {
+            console.error("Błąd podczas pobierania danych kalendarza pacjenta:", error);
+        }
+    };
 
     return (
         <div className={`main-view ${isSidebarOpen ? "sidebar-open" : ""}`}>
@@ -77,22 +117,29 @@ const MainViewDoc = () => {
 
                 <h2>Lista pacjentów:</h2>
                 <div className="patient-field">
-                <ul>
-                    {patients.map((patient, index) => (
-                        <li key={index} onClick={() => handlePatientClick(patient)}>
-                            {patient}
-                        </li>
-                    ))}
-                </ul>
+                    <ul>
+                        {patients.map((patient, index) => (
+                            <li key={index} onClick={() => handlePatientClick(patient)}>
+                                {patient}
+                            </li>
+                        ))}
+                    </ul>
                 </div>
 
                 {selectedPatient && (
                     <div className="patient-modal">
                         <h2>Informacje o pacjencie</h2>
                         <p>
-                            <strong>Imię i nazwisko pacjenta:</strong> {selectedPatient}
+                            <strong>Email pacjenta:</strong> {selectedPatient}
                         </p>
-                        {/* Dodaj inne informacje o pacjencie, jeśli są dostępne */}
+                        <h3>Kalendarz pacjenta:</h3>
+                        <Calendar
+                            localizer={momentLocalizer(moment)}
+                            events={patientEvents}
+                            startAccessor="start"
+                            endAccessor="end"
+                            style={{ height: 500 }}
+                        />
                         <div className="close-button">
                             <button onClick={() => setSelectedPatient(null)}>Zamknij</button>
                         </div>
